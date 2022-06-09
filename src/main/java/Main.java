b146965ebae0a8eb052034e4b511dad7022b8497
mjanusz01 <mikolajjanusz01@gmail.com>
2022-06-09 09:05:52 +0200

    tests1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
        Tests t = new Tests();
        //t.symetricGenerationTest();
-       t.symetricGenerationTest2();
+       t.symetricGenerationTest();
        //t.asymetricGenerationTest();
 
         //MainTest mt = new MainTest();
